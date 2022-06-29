0db18519b9ee6ba1aee2a63182039d66f7af14e0
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-06-29 17:45:51 +0300

    Moved to named arguements

--- index.php
+++ index.php
@@ -3,22 +3,50 @@ $start = microtime(true);
 
 require 'vendor/autoload.php';
 
-$url = $argv[1];
+$short_options = "u:w::";
+$long_options = ["url:", "keyword::"];
+$options = getopt($short_options, $long_options);
 
-$web = new \spekulatius\phpscraper();
 
-$web->go($url);
+if (isset($options["u"]) || isset($options["url"])) {
+    $url = isset($options["u"]) ? $options["u"] : $options["url"];
 
-// Loop through the links
-$links = '';
-foreach ($web->links as $link) {
-    $links .= $link . "\n";
+    $web = new \spekulatius\phpscraper();
+
+    $web->go($url);
+
+    // Loop through the links
+    $links = '';
+    foreach ($web->links as $link) {
+        $links .= $link . "\n";
+    }
+
+
+    $host = parse_url($url, PHP_URL_HOST);
+    $time = round(microtime(true) - $start, 2);
+
+    if (file_put_contents('links.txt', $links)) {
+        echo "Done getting " . count($web->links) . " links for $host in $time Seconds" . PHP_EOL;
+    }
 }
 
+if (isset($options["w"]) || isset($options["keyword"])) {
 
-$host = parse_url($url, PHP_URL_HOST);
-$time = round(microtime(true) - $start, 2);
+    $keyword = strtolower(isset($options["w"]) ? $options["w"] : $options["keyword"]);
 
-if (file_put_contents('links.txt', $links)) {
-    echo "Done getting " . count($web->links) . " links for $host in $time Seconds";
+    $count = 0;
+
+    foreach ($web->paragraphs as $paragraph) {
+
+        if (strpos(strtolower($paragraph), $keyword) !== false) {
+            $count++;
+        } else {
+            $count--;
+        }
+    }
+    if ($count >= 1) {
+        echo "Found $count results for {$keyword}" . PHP_EOL;
+    } else {
+        echo "No results for {$keyword}" . PHP_EOL;
+    }
 }
